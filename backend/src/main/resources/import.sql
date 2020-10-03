d8187401114315b9f7cc98aa85be4ef77285bbae
Avonts <vinialves022@gmail.com>
2020-10-03 20:11:31 -0300

    Auditing data

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-INSERT INTO TB_CATEGORY (name) VALUES ('Books');
-INSERT INTO TB_CATEGORY (name) VALUES ('Eletronics');
-INSERT INTO TB_CATEGORY (name) VALUES ('Computers');
+INSERT INTO TB_CATEGORY (name, created_At) VALUES ('Books', NOW());
+INSERT INTO TB_CATEGORY (name, created_At) VALUES ('Eletronics', NOW());
+INSERT INTO TB_CATEGORY (name, created_At) VALUES ('Computers', NOW());
