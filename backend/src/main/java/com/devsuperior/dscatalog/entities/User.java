efb317c9f630a7cbb86ddc93665fea4cb88a4fcc
Avonts <vinialves022@gmail.com>
2020-11-02 12:40:59 -0300

    User CRUD

--- backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/User.java
@@ -4,7 +4,9 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -24,10 +26,12 @@ public class User implements Serializable{
 	private Long id;
 	private String firstName;
 	private String lastName;
+	
+	@Column(unique = true)
 	private String email;
 	private String password;
 	
-	@ManyToMany
+	@ManyToMany(fetch = FetchType.EAGER)
 	@JoinTable(
 			name = "tb_user_role", 
 			joinColumns = @JoinColumn(name = "user_id"), 
@@ -88,7 +92,7 @@ public class User implements Serializable{
 
 	public Set<Role> getRoles() {
 		return roles;
-	}
+	}	
 
 	@Override
 	public int hashCode() {
